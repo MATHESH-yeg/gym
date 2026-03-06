1f46b452c40a3c0e6aa046df980d0c55f3f6b1f0
Mathesh <matheshwaranmathesh432@gmail.com>
2026-03-06 23:33:47 +0530

    changing backend to render1

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 pool.on('connect', () => {
     console.log('✅ Connected to PostgreSQL');
 });
